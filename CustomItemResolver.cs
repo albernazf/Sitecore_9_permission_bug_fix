8850b786b02fb89bd807dace4285abf05e674cfc
Fernando Albernaz <albernazf@outlook.com>
2017-12-29 18:45:38 +1000

    added files

---
+++ CustomItemResolver.cs
@@ -0,0 +1,98 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Configuration;
+using Sitecore.Data.ItemResolvers;
+using Sitecore.Data.Items;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
+using Sitecore.IO;
+using Sitecore.SecurityModel;
+using Sitecore.Sites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.StringExtensions;
+
+namespace YOUR_NAME_SPACE
+{
+    /// <summary> Hocks into app start of sitecore to add custom code eg. bundles or IoC </summary>
+    public class CustomItemResolver : ItemResolver
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Sitecore.Pipelines.HttpRequest.ItemResolver" /> class.
+        /// </summary>
+        /// <param name="itemManager">The item manager.</param>
+        /// <param name="pathResolver">The path resolver.</param>
+        public CustomItemResolver(BaseItemManager itemManager, ItemPathResolver pathResolver)
+            : this(itemManager, pathResolver, Settings.ItemResolving.FindBestMatch)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Sitecore.Pipelines.HttpRequest.ItemResolver" /> class.
+        /// </summary>
+        /// <param name="itemManager">The item manager.</param>
+        /// <param name="pathResolver">The path resolver.</param>
+        /// <param name="itemNameResolvingMode">The item name resolving mode.</param>
+        protected CustomItemResolver(BaseItemManager itemManager, ItemPathResolver pathResolver, MixedItemNameResolvingMode itemNameResolvingMode)
+            :base(itemManager,pathResolver, itemNameResolvingMode)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Sitecore.Pipelines.HttpRequest.ItemResolver" /> class.
+        /// </summary>
+        [Obsolete("Please use another constructor with parameters")]
+        public CustomItemResolver()
+            : this(ServiceLocator.ServiceProvider.GetRequiredService<BaseItemManager>(), ServiceLocator.ServiceProvider.GetRequiredService<ItemPathResolver>())
+        {
+        }
+
+        public override void Process(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, "args");
+            base.Process(args);
+            if (this.SkipItemResolving(args))
+                return;
+            bool flag = false;
+            Item obj1 = (Item)null;
+            string str = string.Empty;
+            var url = System.Web.HttpContext.Current.Request.Url;
+            var siteContext = Sitecore.Sites.SiteContextFactory.GetSiteContext(url.Host, url.PathAndQuery);
+            try
+            {
+                this.StartProfilingOperation("Resolve current item.", args);
+                HashSet<string> stringSet = new HashSet<string>();
+                foreach (string candidatePath in this.GetCandidatePaths(args))
+                {
+                    if (stringSet.Add(candidatePath))
+                    {
+                        if (this.TryResolveItem(candidatePath, args, out obj1, out flag))
+                        {
+                            str = candidatePath;
+                            break;
+                        }
+                        if (flag)
+                            return;
+                    }
+                }
+                
+                SiteContext site = siteContext;
+                if (obj1 == null || obj1.Name.Equals("*"))
+                {
+                    Item obj2 = this.ResolveByMixedDisplayName(args, out flag);
+                    if (obj2 != null)
+                        obj1 = obj2;
+                }
+                if (obj1 != null || site == null || (flag || !this.UseSiteStartPath(args)) || !this.TryResolveItem(site.StartPath, args, out obj1, out flag))
+                    return;
+                str = site.StartPath;
+            }
+            finally
+            {
+                args.PermissionDenied = flag;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Sitecore_9_permission_bug_fix
-Fix for a bug on sitecore 9
+To use this fix is quite easy, add the class into one of your libraries and add the following patch
+
+The code does exactly the same thing as the sitecore.kernel the only difference is the args.PermissionDenied = flag; which was present on SC 8.2 and got removed on SC 9.0 (initial release)
+
+ps. don't forget to install the nuget Microsoft.Extensions.DependencyInjection.Abstractions.
+
+<configuration xmlns:patch="http://www.sitecore.net/xmlconfig/">
+  <sitecore>
+    <pipelines>
+      <httpRequestBegin>
+        <processor patch:instead="processor[@type='Sitecore.Pipelines.HttpRequest.ItemResolver, Sitecore.Kernel']" type="YOUR_NAME_SPACE.CustomPipelines.CustomItemResolver, YOUR_DLL_NAME" />
+      </httpRequestBegin>
+    </pipelines>
+  </sitecore>
+</configuration>
